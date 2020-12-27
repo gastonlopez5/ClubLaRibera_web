53baa926125741cb11b602fad42610c3206175d0
gastonlopez5 <43733902+gastonlopez5@users.noreply.github.com>
2020-12-27 12:11:37 -0300

    Registro sin terminar

--- wwwroot/script/site.js
+++ wwwroot/script/site.js
@@ -47,17 +47,47 @@ $(function () {
                 alert("Status: " + textStatus); alert("Error: " + errorThrown);
             }  
         })
+    });
 
-        /*
-        $.post(actionUrl, formData).done(function (data) {
-            var newBody = $('.modal-body', data);
-            placeholderElement.find('.modal-body').replaceWith(newBody);
+    placeholderElement.on('change', '[id="Archivo"]', function (event) {
+        event.preventDefault();
 
-            var isValid = newBody.find('[name="IsValid"]').val() == 'True';
-            if (isValid) {
-                placeholderElement.find('.modal').modal('hide');
+        //Get count of selected files
+        var countFiles = $(this)[0].files.length;
+
+        var imgPath = $(this)[0].value;
+        var extn = imgPath.substring(imgPath.lastIndexOf('.') + 1).toLowerCase();
+        var image_holder = $("#imagePreview");
+        image_holder.empty();
+
+        if (extn == "gif" || extn == "png" || extn == "jpg" || extn == "jpeg") {
+            if (typeof (FileReader) != "undefined") {
+
+                //loop for each file selected for uploaded.
+                for (var i = 0; i < countFiles; i++) {
+
+                    var reader = new FileReader();
+                    reader.onload = function (e) {
+                        $("<img />", {
+                            "src": e.target.result,
+                            "class": "thumb-image",
+                            "width": "200px",
+                            "height": "auto"
+                        }).appendTo(image_holder);
+                    }
+
+                    image_holder.show();
+                    reader.readAsDataURL($(this)[0].files[i]);
+                }
+
+            } else {
+                alert("This browser does not support FileReader.");
             }
-        });
-        */
+        } else {
+            //alert("Pls select only images");
+            alertify.set('notifier', 'position', 'top-center');
+            alertify.error('Por favor, seleccione solo imagenes');
+        }
     });
 });
+
